70a200b9e074e64e3e064a1c2345d83f4b571af4
STUD <breath44@yande.ru>
2022-07-04 17:33:04 +0300

    Initial commit

---
+++ clean_code.py
@@ -0,0 +1,19 @@
+﻿import logging.config
+from settings import logger_config
+
+
+logging.config.dictConfig(logger_config)
+logger = logging.getLogger('app_logger.'+ __name__)
+
+
+def main():
+    logger.debug(r'это консоль')
+    logger.info(r'это консоль и файл')
+    logger.warning(r'это консоль файл телега')
+    logger.error(r'это консоль файл телега мыло')
+    print(10 + 15)
+
+
+if __name__ == '__main__':
+    main()
+    assert passw_stranght('') == 'Too Weak'
